2371745d9264ae314d26429bc21e9ca0a0264c2e
Paul Gerasymchuk <pgerasymchuk@gmail.com>
2025-04-22 16:51:17 +0300

    parallelize checking negative cycles

--- src/BellmanFordParallel.java
+++ src/BellmanFordParallel.java
@@ -1,7 +1,7 @@
 import java.util.*;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 //import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BellmanFordParallel implements ShortestPathFinder {
@@ -21,59 +21,73 @@ public class BellmanFordParallel implements ShortestPathFinder {
 
         long t1 = System.nanoTime();
 
-        //int verticesPerGroup = g.V / numThreads;
         byte[] groupNumberOfEdges = new byte[g.E];
         for (int i = 0; i < g.E; i++) {
-            //groupNumberOfEdges[i] = (byte)((g.edges.get(i).destination() / verticesPerGroup) % numThreads);
             groupNumberOfEdges[i] = (byte)(g.edges.get(i).destination() % numThreads);
         }
 
         long t2 = System.nanoTime();
         System.out.println("Initializing, ms: " + (t2 - t1) * 1e-6);
 
+        ArrayList<Callable<Void>> tasks = new ArrayList<>();
+
         for (int i = 0; i < g.V - 1; i++) {
             //changes.set(false);
 
-            List<Future<?>> futures = new ArrayList<>();
-
             for (int j = 0; j < numThreads; j++) {
                 int finalJ = j;
-                futures.add(executor.submit(() -> {
+                tasks.add(() -> {
                     //boolean localChange = false;
                     for (int k = 0; k < g.E; k++) {
                         if (groupNumberOfEdges[k] == finalJ) {
                             Graph.Edge edge = g.edges.get(k);
+                            int u = edge.source();
+                            int v = edge.destination();
+                            int w = edge.weight();
 
-                            if (distances[edge.source()] != Integer.MAX_VALUE &&
-                                    distances[edge.source()] + edge.weight() < distances[edge.destination()]) {
-
-                                distances[edge.destination()] = distances[edge.source()] + edge.weight();
-                                predecessors[edge.destination()] = edge.source();
+                            if (distances[u] != Integer.MAX_VALUE && distances[u] + w < distances[v]) {
+                                distances[v] = distances[u] + w;
+                                predecessors[v] = u;
                                 //localChange = true;
                             }
                         }
                     }
                     //if (localChange) { changes.set(true); }
-                }));
+                    return null;
+                });
             }
 
-            for (Future<?> future : futures) {
-                try {
-                    future.get();
-                } catch (Exception e) { throw new RuntimeException(e); }
-            }
+            try {
+                executor.invokeAll(tasks);
+                tasks.clear();
+            } catch (InterruptedException e) { throw new RuntimeException(e); }
 
             //if (!changes.get()) { System.out.printf("parallel break at %s iteration\n", i); break; } // no updates in this iteration
         }
 
-        for (Graph.Edge edge : g.edges) {
-            int u = edge.source();
-            int v = edge.destination();
-            int w = edge.weight();
-            if (distances[u] != Integer.MAX_VALUE && distances[u] + w < distances[v]) {
-                throw new IllegalArgumentException("Graph contains a negative-weight cycle");
-            }
+        for (int j = 0; j < numThreads; j++) {
+            int finalJ = j;
+            tasks.add(() -> {
+                for (int k = 0; k < g.E; k++) {
+                    if (groupNumberOfEdges[k] == finalJ) {
+                        Graph.Edge edge = g.edges.get(k);
+                        int u = edge.source();
+                        int v = edge.destination();
+                        int w = edge.weight();
+
+                        if (distances[u] != Integer.MAX_VALUE && distances[u] + w < distances[v]) {
+                            throw new IllegalArgumentException("Graph contains a negative-weight cycle");
+                        }
+                    }
+                }
+                return null;
+            });
         }
+
+        try {
+            executor.invokeAll(tasks);
+            tasks.clear();
+        } catch (InterruptedException e) { throw new RuntimeException(e); }
 
         executor.shutdown();
 
